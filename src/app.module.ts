413d435c1642f28a56eabb87a3ac8e5de9f7a1bb
Milan <milan@digidi.co>
2026-04-06 10:32:31 +0530

    Fix script issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
       isGlobal: true,
       load: [configuration],
       validate,
-      envFilePath: '../.env',
+      envFilePath: '.env',
     }),
     PrismaModule,
     AuthModule,
